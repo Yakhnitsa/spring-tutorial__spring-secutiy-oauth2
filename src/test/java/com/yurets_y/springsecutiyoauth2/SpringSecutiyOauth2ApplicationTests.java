fb1a8e681e2f3fbf3e938a56a7e4db387922d571
Yuriy Yakhnitsa <transpmailbox@gmail.com>
2019-12-03 12:43:55 +0200

    Initial commit Simple facebook authentification added

---
+++ src/test/java/com/yurets_y/springsecutiyoauth2/SpringSecutiyOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurets_y.springsecutiyoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutiyOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
